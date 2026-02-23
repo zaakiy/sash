f8e8d1abff03238ca477c746a6e41d5c5148916e
Zak Siddiqui <zak@kelsiem.com>
2026-02-23 22:42:33 +1000

    place docker upgrade into a function

--- sash.sh
+++ sash.sh
@@ -1,3 +1,4 @@
 # Source countdown timer function
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "${SCRIPT_DIR}/countdown.sh"
+source "${SCRIPT_DIR}/docker-upgrade-containers.sh"
