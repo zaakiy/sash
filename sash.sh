4a246c4f2e14220b3f02babdf592abfd6c12d297
Zak Siddiqui <zak@kelsiem.com>
2026-02-25 01:19:37 +1000

    copied from devmachine

--- sash.sh
+++ sash.sh
@@ -1,4 +1,3 @@
 # Source countdown timer function
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "${SCRIPT_DIR}/countdown.sh"
-source "${SCRIPT_DIR}/docker-upgrade-containers.sh"
